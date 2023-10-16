d79d6fcc80e40a2151897849b69de113436dd870
mcindevbr <marcelocavalcanti@mcindev.com.br>
2023-10-15 22:09:21 -0300

    alteração na seção sobre

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*------------------- typed js efeito maquina de escrever --------------------*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Analista de Sistemas'],
+    strings: ['Analista de Sistemas', 'Desenvolvedor Web'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
